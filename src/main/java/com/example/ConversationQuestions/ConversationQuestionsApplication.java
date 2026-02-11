aa36de4084daaf9eb5f6f4253d5e6702fd58ec40
MorelliCode <110130671+MorelliCode@users.noreply.github.com>
2026-02-11 11:23:38 -0300

    Initial commit

---
+++ src/main/java/com/example/ConversationQuestions/ConversationQuestionsApplication.java
@@ -0,0 +1,13 @@
+package com.example.ConversationQuestions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConversationQuestionsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConversationQuestionsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/ConversationQuestions/ConversationQuestionsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ConversationQuestions;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConversationQuestionsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
