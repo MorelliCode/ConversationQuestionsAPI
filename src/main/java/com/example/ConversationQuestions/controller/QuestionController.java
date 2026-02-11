e08400182e3fb5c046afc8dee9f99010234bef18
MorelliCode <110130671+MorelliCode@users.noreply.github.com>
2026-02-11 12:09:47 -0300

    Add QuestionController class

---
+++ src/main/java/com/example/ConversationQuestions/controller/QuestionController.java
@@ -0,0 +1,29 @@
+package com.example.ConversationQuestions.controller;
+
+import com.example.ConversationQuestions.model.ConversationQuestion;
+import com.example.ConversationQuestions.service.QuestionService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/questions")
+public class QuestionController {
+    private final QuestionService questionService;
+
+    public QuestionController(QuestionService questionService) {
+        this.questionService = questionService;
+    }
+
+    @GetMapping
+    public List<ConversationQuestion> getAll() {
+        return questionService.getAllQuestions();
+    }
+
+    @GetMapping
+    public ConversationQuestion getRandom() {
+        return questionService.getRandomQuestion();
+    }
+}
